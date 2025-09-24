258e6b69f46914a77400c889261a510ef7b3962d
sungwookcho@LAPTOP <fdsa7563@gmail.com>
2025-09-24 17:57:19 +0900

    함수호출, jump, return, STACK segment

--- 메인.cpp
+++ 메인.cpp
@@ -13,6 +13,8 @@
 //	y = temp;
 //}
 
+void change(int*, int*); // 선언은 헤더 파일에서.
+
 void change(int* x, int* y)
 {
 	int temp = *x;
@@ -27,7 +29,7 @@ int main()
 	int a { 1 }, b { 2 };
 
 	// 여기에서 a와 b의 값을 서로 바꾸는 코딩을 하라. (직접)
-	change(&a, &b);
+	change(&a, &b); // change 함수로 jump한다. 실제로 cpu에겐 jump 명령어가 존재한다.
 
 	std::cout << a << ", " << b << '\n'; // 2, 1이 출력되어야 한다.
 
