352d459b8e9cb404e569ac67654989eb53040cdc
sungwookcho@LAPTOP <fdsa7563@gmail.com>
2025-09-29 18:24:48 +0900

    레퍼런스 4 - 정렬, change 함수

--- 메인.cpp
+++ 메인.cpp
@@ -1,29 +1,67 @@
 ﻿//--------------------------------------------------------------------
 // C++에서 포인터 대신 사용할 수 있는 레퍼런스(reference)
-// 레퍼런스 - 메모리를 차지한 객체에 붙이는 별명
-// 왜? - 포인터를 잘 이해하는 프로그래머가 편하고 쉽게 다른 지역의 메모리에 
-//       access하기 위한 도구
 //--------------------------------------------------------------------
 
 #include <iostream>
+#include <random>
+#include <print>
 #include "save.h"
+
+void change(int*, int*); // 선언
+void change(int&, int&); 
+
+void change(int* x, int* y)
+{
+	int temp { *x };
+	*x = *y;
+	*y = temp;
+}
+
+void change(int& x, int& y)
+{
+	int temp { x };
+	x = y;
+	y = temp;
+}
+
+
+// [문제] 랜덤값(0~99'999)을 갖는 int 100'000개를 만들어라.
+// 오름차순으로 정렬하라.
+// 정렬 결과를 한 줄(80글자)에 10개씩 화면에 출력하라.
+
+
+// ===== 교수님 정답 ======
+
+
+std::default_random_engine dre { std::random_device()() };
+std::uniform_int_distribution uid { 0, 99999 };
+
+const int SIZE { 10'000 };
 
 //--------
 int main()
 //--------
 {
-	int a { 1 };
-	int& r { a };
 
-	// [문제] a와 r의 정체를 파악해주세요.
+	int a[SIZE];
+	for (int i = 0; i < SIZE; ++i) {
+		a[i] = uid(dre);
+	}
 
-	std::cout << "a의 메모리 크기 - " << sizeof a << "\n";
-	std::cout << "a의 메모리 주소 - " << std::addressof(a) << "\n";
-	std::cout << "a의 데이터 타입 - " << typeid(a).name() << "\n\n";
+	// [문제] a의 값을 오름차순으로 정렬하시오
+	// 버블정렬
 
-	std::cout << "r의 메모리 크기 - " << sizeof r << "\n";
-	std::cout << "r의 메모리 주소 - " << std::addressof(r) << "\n";
-	std::cout << "r의 데이터 타입 - " << typeid(r).name() << "\n\n";
+	for (int i = 0; i < SIZE - 1; ++i) {
+		for (int j = 0; j < SIZE - 1 - i; ++j) {
+			if (a[j] > a[j + 1])
+				change(a[j], a[j + 1]);		// call by reference
+		}
+	}
+
+	for (int num : a) {
+		std::print("{:8}", num);
+	}
+	std::cout << '\n';
 
 	save("메인.cpp");
 }
