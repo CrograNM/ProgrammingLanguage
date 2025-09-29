8df5b4f8dbd5b504a0bff089b8e3d6b42a3dc7e7
sungwookcho@LAPTOP <fdsa7563@gmail.com>
2025-09-29 18:06:17 +0900

    레퍼런스 1

--- 메인.cpp
+++ 메인.cpp
@@ -1,63 +1,28 @@
 ﻿//--------------------------------------------------------------------
-// 두 변수의 값을 교환하라
-// -> pointer, reference --> 원격 메모리를 조작할 수 있는 도구
-//--------------------------------------------------------------------
-// 포인터 - 어떤 지역에서 다른 지역에 있는 메모리를 access 할 때 사용
+// 레퍼런스 - 
 //--------------------------------------------------------------------
 
 #include <iostream>
-#include <random>
-#include <print>
 #include "save.h"
-
-void change(int* x, int* y)
-{
-	int temp = *x;
-	*x = *y;
-	*y = temp;
-}
-
-
-// [문제] 랜덤값(0~99'999)을 갖는 int 100'000개를 만들어라.
-// 오름차순으로 정렬하라.
-// 정렬 결과를 한 줄(80글자)에 10개씩 화면에 출력하라.
-
-
-// ===== 교수님 정답 ======
-
-
-std::default_random_engine dre { std::random_device()() };
-std::uniform_int_distribution uid { 0, 99999 };
-
-const int SIZE { 10'0000 }; 
-// const : 읽기만 가능하게 만드는 자격 부여 (Read Only)
 
 //--------
 int main()
 //--------
 {
+	int a { 1 }; 
+	int& r { a };  
 
-	int a[SIZE];
-	for (int i = 0; i < SIZE; ++i) {
-		a[i] = uid(dre);
-	}
+	// [문제] a와 r의 정체를 파악해주세요.
 
-	// [문제] a의 값을 오름차순으로 정렬하시오
-	// 버블정렬
+	std::cout << "== a의 정체 ==" << '\n'; 
+	std::cout << "a의 메모리 크기 - " << sizeof a << "\n"; 
+	std::cout << "a의 메모리 주소 - " << std::addressof(a) << "\n";
+	std::cout << "a의 데이터 타입 - " << typeid(a).name() << "\n\n";
 
-	for (int i = 0; i < SIZE - 1; ++i)
-	{
-		for (int j = 0; j < SIZE - 1 - i; ++j)
-		{
-			if (a[j] > a[j + 1])
-				change(&a[j], &a[j + 1]);
-		}
-	}
-	
-	for (int num : a) {
-		std::print("{:8}", num);
-	}
-	std::cout << '\n';
+	std::cout << "== r의 정체 == " << '\n';
+	std::cout << "r의 메모리 크기 - " << sizeof r << "\n";
+	std::cout << "r의 메모리 주소 - " << std::addressof(r) << "\n";
+	std::cout << "r의 데이터 타입 - " << typeid(r).name() << "\n\n";
 
 	save("메인.cpp");
 }
