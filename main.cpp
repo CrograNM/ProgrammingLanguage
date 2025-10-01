53fafb5318912195e65983da3ffb1ef415a19013
sungwookcho@LAPTOP <fdsa7563@gmail.com>
2025-10-01 19:05:33 +0900

    == 1001 수업 끝

--- main.cpp
+++ main.cpp
@@ -1,49 +1,29 @@
 ﻿//--------------------------------------------------------------------
 // 10. 27 월요일 - 중간시험 
 //--------------------------------------------------------------------
-// C++에서 포인터 대신 사용할 수 있는 레퍼런스(reference)
+// 동적할당(Dynamic Allocation) - dynamic은 run-time으로 해석해야 함
 //--------------------------------------------------------------------
 
 #include <iostream>
 #include "save.h"
 
-// <방법> : qsort가 요구하는 형식에 맞는 비교함수
-// 리턴은 반드시 int를 리턴하는 형식으로 약속
-// 인자는 const void* 형식의 포인터 2개를 받도록 약속
-int 방법(const void* a, const void* b) 
-{
-	return *(char*)a - *(char*)b; 
-	// 양수, 음수, 0을 리턴하는 형식으로 한줄로 작성 가능
-	// 내림차순이면 a, b 순서를 바꾸면 됨
-}
+// [문제] 사용자가 원하는 숫자만큼 int를 저장할 메모리를 확보한다.
+// 그 메모리를 1부터 시작하는 정수로 채워라.
+// 메모리의 합계를 화면에 출력하여라.
 
 //--------
 int main()
 //--------
 {
-	char pangram[] {"The quick brown fox jumps over the lazy dog"};
+	std::cout << "int 몇 개가 필요한가요? ";
+	int num;
+	std::cin >> num;
 
-	// [문제] pangram을 오름차순으로 qsort를 사용하여 정렬하고 출력하라.
-
-	qsort(pangram, sizeof pangram - 1, 1, 방법);
-
-	// generic function : 어떤 타입이든 다 처리할 수 있는 함수
-	// 프로그래밍에서 generic 이란, 타입을 가리지 않고 다 처리할 수 있는 것을 의미
-
-	// C/C++ -> Procedural Programming
-	// C with Classes -> C++의 원래 정의 Object Oriented Programming
-	
-	// C++의 현재 정의
-	// Generic Programming Language			
-	// Functional Programming Language
-	// Meta Programming Language 
-
-	// OOP 와 GP 는 관점이 다르다.
-
-	// qsort는 Generic Programming의 시작점이라고 볼 수 있음.
-	// 자료형에 관심이 없는 함수
-
-	std::cout << pangram << '\n';
+	int*p = new int[num];
+	for (int i = 0; i < num; ++i)
+		p[i] = i + 1;
+	for (int i = 0; i < num; ++i)
+		std::cout << p[i] << ' ';
 
 	save("main.cpp");
 }
