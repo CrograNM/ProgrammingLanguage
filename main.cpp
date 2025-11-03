339d19a7c12922e3ff114a1c0b74befa9d40f4b0
sungwookcho@LAPTOP <fdsa7563@gmail.com>
2025-11-03 18:22:14 +0900

    생성자 소멸자

--- main.cpp
+++ main.cpp
@@ -12,15 +12,22 @@ using namespace std;
 
 class Dog {
 public:
-	Dog()
-	{ // special function - default constructor -> default ctor (creator 줄여서)
+	Dog() { // special function - default constructor -> default ctor (creator 줄여서)
 		name = "디폴트";
 	}
-
 	// Dog(string) 생성자
 	Dog(string this_name) : name { this_name } {
+		cout << "생성자(string) 호출되었다. " << endl;
 	}
-	
+
+	~Dog() { // special function - destructor
+		// 소멸자 - 객체가 소멸될 때 호출되는 함수
+		// 소멸자는 단 하나만 존재한다.
+		cout << "소멸자가 호출되었다." << endl;
+	}
+
+	// 생성자와 소멸자는 메모리를 색칠하는거지, 직접 메모리를 해제하는건 아님
+		
 	void bark() {
 		cout << name << " 입니다멍" << endl;
 	}
@@ -34,8 +41,8 @@ private: // access modifier - class의 default (private)
 int main()
 //--------
 {
-	Dog dogs[3] = { string{"코코"}, "보리"s, "콩이"s }; // s: string literal, 연산자 오버로딩
-	for ( Dog dog : dogs )
-		dog.bark();
 	save("main.cpp");
+	cout << "------------ 메인 시작 ------------" << endl;
+	Dog dog = { "콩이"s }; 
+	cout << "------------ 메인 끝 -------------" << endl;
 }
