40231558fc9c91030326573627493661ed9c6435
sungwookcho@LAPTOP <fdsa7563@gmail.com>
2025-11-26 18:47:42 +0900

    StarString 예제

--- main.cpp
+++ main.cpp
@@ -2,6 +2,8 @@
 // 12.15 월요일(15주 1일) - 기말시험(다형성)
 // ----------------------------------------------------------------------
 // class 간의 관계 - 상속(Inheritance)
+// - 코드 재활용
+// - 다형성(Polymorphism) 구현하려고
 // ----------------------------------------------------------------------
 
 #include <iostream>
@@ -11,21 +13,29 @@
 
 using namespace std;
 
-class A {
-	// B b;					// A has-a B
+class StarString : public string { 
+private:
+	friend ostream& operator<<(ostream& os, const StarString& ss) { 
+		for ( const char& c : ss) {
+			if (isdigit(c)) 
+				os << '*';
+			else 
+				os << c;
+		}
+		return os;
+	}
 };
-
-class B : A {				// B is-a A
-};
-
-class Animal { };
-class Dog : Animal { };		// Dog is-a Animal
-
 
 //--------
 int main()
 //--------
 {
+	string str { "2025년 11월 26일 - 상속하면 뭐가 좋은가" };
+
+	// [과제] str을 화면에 출력하는데 숫자는 '*'로 바꿔라.
+	StarString ss { str };
 	
+	cout << ss << endl;
+
 	save("main.cpp");
 }
