db5e73f7afa7633e8edcf1ac794145c78c286536
sungwookcho@LAPTOP <fdsa7563@gmail.com>
2025-12-01 18:46:04 +0900

    Dog speed 정렬 - 교수님 답 1
    멤버 변수 speed 초기화를 랜덤으로 할 수 있다

--- main.cpp
+++ main.cpp
@@ -14,18 +14,15 @@
 
 using namespace std;
 
-default_random_engine dre { random_device{}() };
-uniform_int_distribution<int> uid_speed { 1, 10000 };
-uniform_int_distribution<int> uid_name { 1, 10000 };
+default_random_engine dre { 20251201 };
+uniform_int_distribution<int> uid { 1, 10'000 };
 
 class Animal {
 public:
-	Animal() { 
-		name = "Animal_" + to_string(uid_name(dre));
-	}
+	Animal() = default;
 	Animal(const char* n) : name(n) { }
 
-	void move() const { 
+	void move() { 
 		cout << name << " - 움직이는 중" << endl;
 	}
 
@@ -35,28 +32,18 @@ protected:	// 상속 때문에 만들 수 밖에 없었던 접근 지정자
 
 class Dog : public Animal {
 public:
-	Dog() { 
-		speed = uid_speed(dre);
-	}
-	Dog(const char* n) : Animal(n)
-	{
+	Dog() = default;
+	Dog(const char* n) : Animal(n) {
 	}
 
 	// member function overriding -> 오버로딩과 차이점은? 
-	void move() const {
+	void move() {
 		cout << name << " 난 달리기 선수 - " << speed << endl;
 	}
 
-	int getSpeed() const { return speed; }
-
 private:
-	int speed;
+	int speed { uid(dre) };
 };
-
-int 속도기준오름차순(const void* a, const void* b)
-{
-	return ((const Dog*)a)->getSpeed() - ((const Dog*)b)->getSpeed();
-}
 
 //--------
 int main()
@@ -65,19 +52,15 @@ int main()
 	// [문제] 문제없이 실행되도록 필요한 코드를 추가하고 답지에 추가한 내용을 적어라.
 	Dog dogs[1000];				// 여기서 speed 값은 [1, 10'000] 랜덤값으로 설정한다.
 
-
-	// speed 기준 오름차순으로 정렬
-	qsort(dogs, 1000, sizeof(Dog), 속도기준오름차순);
-
-	/*
-	for (const Dog& dog : dogs) {
+	for (const Dog& dog : dogs) { 
 		dog.move();
 	}
-	*/
+
+	// speed 기준 오름차순으로 정렬
+	// qsort(dogs, 1000, sizeof(Dog), 속도기준오름차순);
 	
 	// 가장 빠른 dog의 정보를 화면에 출력
-	dogs[999].move();
-	
+	// dogs[999].move();
 
-	save("main.cpp");
+	// save("main.cpp");
 }
