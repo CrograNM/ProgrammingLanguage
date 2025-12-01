521fa4b6e2b94e6dbfcf5c9f403d5c52bd7d91f4
sungwookcho@LAPTOP <fdsa7563@gmail.com>
2025-12-01 19:04:55 +0900

    == 1201 수업 종료

--- main.cpp
+++ main.cpp
@@ -4,23 +4,20 @@
 // class 간의 관계 - 상속(Inheritance)
 // - 코드 재활용
 // - 다형성(Polymorphism) 구현하려고
+//		-> One Interface, Multiple Behaviors
 // ----------------------------------------------------------------------
 
 #include <iostream>
 #include <string>
-#include <random>
 
 #include "save.h"
 
 using namespace std;
 
-default_random_engine dre { 20251201 };
-uniform_int_distribution<int> uid { 1, 10'000 };
-
 class Animal {
 public:
 	Animal() = default;
-	Animal(const char* n) : name(n) { }
+	Animal(const char* n) : name{ n } { }
 
 	void move() { 
 		cout << name << " - 움직이는 중" << endl;
@@ -44,30 +41,19 @@ public:
 	int getSpeed() const {
 		return speed;
 	}
-
 private:
-	int speed { uid(dre) };
+	int speed { };
 };
 
-int 스기오(const void* a, const void* b) 
-{ 
-	// return ((const Dog*)a)->getSpeed() - ((const Dog*)b)->getSpeed();
-	return static_cast<const Dog*>(a)->getSpeed() - static_cast<const Dog*>(b)->getSpeed();
-}
+class Bird : public Animal {
+};
 
 //--------
 int main()
 //--------
 {
-	// [문제] 문제없이 실행되도록 필요한 코드를 추가하고 답지에 추가한 내용을 적어라.
-	Dog dogs[1000];				// 여기서 speed 값은 [1, 10'000] 랜덤값으로 설정한다.
-
-
-	// speed 기준 오름차순으로 정렬
-	qsort(dogs, 1000, sizeof(Dog), 스기오);
-	
-	// 가장 빠른 dog의 정보를 화면에 출력
-	dogs[1000 - 1].move();
+	Bird bird("토리");
+	bird.move(); // 토리 - 난다
 
 	// save("main.cpp");
 }
