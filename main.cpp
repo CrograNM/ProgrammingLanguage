7c4b7db00cfbb0b9014cf5bceda5765c537afd08
sungwookcho@LAPTOP <fdsa7563@gmail.com>
2025-12-08 18:51:41 +0900

    virtual 키워드
    길잡이 역할

--- main.cpp
+++ main.cpp
@@ -16,41 +16,33 @@ using namespace std;
 
 class Animal {
 public:
-	Animal() = default;
-	Animal(const char* n) : name{ n } { }
-
-	virtual void move() const { 
-		cout << name << " - 움직이는 중" << endl;
+	virtual ~Animal()
+	{	// 길잡이 : virtual destructor
+		cout << "Animal 소멸" << endl;
 	}
 
-	virtual ~Animal() = default;
-
-protected:	// 상속 때문에 만들 수 밖에 없었던 접근 지정자
-	string name;
+	virtual void move() const = 0; // pure virtual function
 };
 
 class Dog : public Animal {
 public:
-	Dog() = default;
-	Dog(const char* n) : Animal(n) {
+	~Dog () {
+		cout << "Dog 소멸" << endl;
 	}
 
-	// member function overriding -> 오버로딩과 차이점은? 
-	void move() const override {
-		cout << name << "Dog 달린다" << endl;
+	virtual void move() const override {
+		cout << "Dog 달린다" << endl;
 	}
-
-	int getSpeed() const {
-		return speed;
-	}
-private:
-	int speed { };
 };
 
 class Bird : public Animal {
 public:
-	void move() const override {
-		cout << name << "Bird 난다" << endl;
+	~Bird () {
+		cout << "Bird 소멸" << endl;
+	}
+
+	virtual void move() const override {
+		cout << "Bird 난다" << endl;
 	}
 };
 
@@ -66,22 +58,45 @@ uniform_int_distribution<int> uid { 0, 1 };
 int main()
 //--------
 {
-	cout << "관리할 동물 수는?";
+	cout << "관리할 동물 수는? ";
 	int num;
 	cin >> num;
 
 	Animal** 우리 = new Animal * [num];	// Animal** : Animal* 배열의 포인터(주소)
-	for (int i = 0; i < num; ++i) { 
+	for (int i = 0; i < num; ++i) {
 		if (0 == uid(dre)) {
 			우리[i] = new Dog;
-		}
-		else {
+		} else {
 			우리[i] = new Bird;
 		}
 	}
+
+	// [문제] 동물농장 사업이 너무 잘된다. 농장을 2배로 늘리려 한다.
+	// 우리에 있는 모든 동물을 새 우리로 모두 옮겨라.
+	// 그리고 Dog를 num + 1 번째 동물로 채워 넣어라.
+	// 이때 다형성 코드는 변하지 않아야 한다.
+
+	Animal** 새집 = new Animal * [num * 2];
+	for (int i = 0; i < num; ++i) {
+		새집[i] = 우리[i];	// 기존 동물들 옮기기 (포인터 복사)
+	}
+	delete[] 우리;			// 기존 우리 메모리 해제
+	우리 = 새집;				// 우리 포인터를 새집으로 변경
+
+	우리[num] = new Dog;		// 새로운 Dog 추가
+	num += 1;
+
+	for (int i = 0; i < num; ++i) {
+		우리[i]->move();		// 다형성 구현 확인
+	}
 	
-	for ( int i = 0; i < num; ++i) 
-		우리[i]->move();		 // 다형성 구현 확인
-	
+	// [문제] 끝나기 전에 메모리를 확실하게 정리하였나?
+	// 당신은 어떻게 확인하였나요?
+
+	for (int i = 0; i < num; ++i) {
+		delete 우리[i];		// 각 동물 객체 메모리 해제
+	}
+	delete[] 우리;		// 우리 배열 메모리 해제
+
 	save("main.cpp");
 }
